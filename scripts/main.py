be11bcdefe6add932c37aeee9f5172c789a936f9
José P. Gómez B. S <jpablo.gomezb@hotmail.com>
2022-11-20 11:53:01 +0100

    Fixed string to send data to ezdata

--- scripts/main.py
+++ scripts/main.py
@@ -11,7 +11,7 @@ from gps_simple import *
 
 setScreenColor(0x111111)
 
-title0 = M5Title(title="GeoSmart Button", x=3, fgcolor=0xFFFFFF, bgcolor=0x0000FF)
+title0 = M5Title(title="Georeporting Btn.", x=3, fgcolor=0xFFFFFF, bgcolor=0x0000FF)
 wifi_logs = M5TextBox(130, 30, "Wifi -", lcd.FONT_Default, 0xFFFFFF, rotate=90)
 btn_indicator = M5Circle(67, 180, 32, 0xcccccc, 0xcccccc)
 gps_logger = M5TextBox(15, 30, "GPS:", lcd.FONT_Default, 0xFFFFFF, rotate=90)
@@ -27,7 +27,7 @@ M5Led.off()
 def set_app():
     setScreenColor(0x111111)
     get_wifi_status()
-    title0 = M5Title(title="GeoSmart Button", x=3, fgcolor=0xFFFFFF, bgcolor=0x0000FF)
+    title0 = M5Title(title="Georeporting Btn.", x=3, fgcolor=0xFFFFFF, bgcolor=0x0000FF)
     wifi_logs = M5TextBox(130, 30, "", lcd.FONT_Default, 0xFFFFFF, rotate=90)
     gps_logger = M5TextBox(15, 30, "GPS:", lcd.FONT_Default, 0xFFFFFF, rotate=90)
     btn_indicator = M5Circle(67, 180, 32, 0xcccccc, 0xcccccc)
@@ -43,7 +43,7 @@ def btn_a_was_pressed():
         # sending data for testing here:
         # https://ezdata.m5stack.com/share/?type=table&sid=mgIWDt2YyyUoj72Qj87X08oYRhUrraPb
         ezdata.setData('C6J5te2z8Qfr5rkLPXCeRUkyjW3kSAgB', 'geolocation',
-                       str(gps_0.latitude, {gps_0.latitude}))
+                       str(gps_0.latitude, gps_0.latitude))
         btn_indicator.setBgColor(0x33ff33)
         geo_status.setText("GPS location saved")
     except Exception as ex:
